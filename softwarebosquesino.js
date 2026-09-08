edc5c4399fe14a0fd889ebe2e4dfbebd4755808e
giraldoramirezjuan-coder <giraldoramirezjuan@gmail.com>
2026-09-07 19:27:02 -0500

    Add files via upload

--- softwarebosquesino.js
+++ softwarebosquesino.js
@@ -113,7 +113,7 @@ app.get('/', (req, res) => {
 });
 
 // --- 5. INICIO DEL SERVIDOR ---
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor en línea en el puerto ${PORT}`);
 });
